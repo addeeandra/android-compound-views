51dab5ad162bf18ed8c5c1cdc50fe5ee2b7d8f4c
Aditya Chandra <inibukanadit@gmail.com>
2019-09-14 05:02:58 +0700

    GreetingView added!

--- app/src/main/java/dev/poteto/app/compoundview/views/GreetingView.kt
+++ app/src/main/java/dev/poteto/app/compoundview/views/GreetingView.kt
@@ -1,7 +1,40 @@
 package dev.poteto.app.compoundview.views
 
-class GreetingView {
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import dev.poteto.app.compoundview.R
+import kotlinx.android.synthetic.main.view_greeting.view.*
 
-    // TODO greeting view implementation
+class GreetingView(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    init {
+        inflateView()
+        initAttributes(context.obtainStyledAttributes(attrs, R.styleable.GreetingView))
+    }
+
+    private fun inflateView() {
+        LayoutInflater.from(context).inflate(R.layout.view_greeting, this)
+    }
+
+    private fun initAttributes(attrs: TypedArray?) {
+        attrs?.let { attr ->
+            tv_greeting_hello.text = attr.getText(R.styleable.GreetingView_greet_text)
+            tv_greeting_name.text = attr.getText(R.styleable.GreetingView_greet_name)
+            tv_greeting_explore.text = attr.getText(R.styleable.GreetingView_greet_challenges)
+            iv_greeting_background.setImageResource(
+                attr.getResourceId(
+                    R.styleable.GreetingView_greet_background,
+                    R.drawable.img_dummy_2
+                )
+            )
+        }
+    }
 
 }
